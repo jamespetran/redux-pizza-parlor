2b487308453908c667e48b87d1a81f43632e7fb6
Jacob Larson <jacob.larson55112@gmail.com>
2022-01-20 12:57:35 -0600

    setting up admin page

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux'; 
+import logger from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<App />, document.getElementById('root'));
