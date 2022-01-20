98f7e6f1b95f5defd7e89c77dc1796f7b2a1dcdb
jeremy-cady <jeremy.cady@outlook.com>
2022-01-20 11:12:54 -0600

    store created - customerInfoReducer created - started fleshing out CustomerForm

---
+++ src/components/CustomerForm/CustomerForm.jsx
@@ -0,0 +1,86 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+
+function CustomerForm() {
+    console.log('in CustomerForm');
+
+    // setup instances of dispatch and history
+    // to be able to talk to state and advance the page to
+    // checkout page after clicking the 'Next' button 
+    const dispatch = useDispatch();
+    const history = useHistory();
+
+    // setup local state to store customer objects
+    const [customerInfo, setCustomerInfo] = useState({
+        name: '',
+        streetAddress: '',
+        city: '',
+        zip: ''
+    });
+
+    const handleInputChange = (event) => {
+        setCustomerInfo({
+            // spread operator handles all user inputs
+            ...customerInfo, [event.target.name]: event.target.value,
+        });
+    } // end handleInputChange
+
+
+
+    return (
+        <>
+            <h1>Step 2: Customer Information</h1>
+
+            <form>
+                <input
+                    type="text"
+                    placeholder="Name"
+                    value={customerInfo.name}
+                    onChange={handleInputChange}    
+                />
+
+                <br></br>
+
+                <input
+                    type="text"
+                    placeholder="Street Address"
+                    value={customerInfo.streetAddress}
+                    onChange={handleInputChange}    
+                />
+
+                <br></br>
+
+                <input
+                    type="text"
+                    placeholder="City"
+                    value={customerInfo.city}
+                    onChange={handleInputChange}    
+                />
+
+                <br></br>
+
+                <input
+                    type="text"
+                    placeholder="Zip Code"
+                    value={customerInfo.zip}
+                    onChange={handleInputChange}    
+                />
+
+                <br></br>
+
+                <input
+                    type="radio"
+                    name="pickup"
+                    value=
+                />
+
+            </form>
+        </>
+        
+    )
+
+}
+
+
+export default CustomerForm;
--- src/index.js
+++ src/index.js
@@ -2,5 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+const customerInfoReducer = (state = [], action) => {
+    switch (action.type) {
+        case 'ADD_CUSTOMER_INFO':
+            return [...state, action.payload];
+    }
+    return state;
+}
+
+
+
+
+
+
+// Store
+const storeInstance = createStore (
+    combineReducers({
+        customerInfoReducer
+    }),
+    applyMiddleware(logger)
+);
+
+
+
+
+ 
+ReactDOM.render(
+    <Provider>
+        <App />
+    </Provider>, 
+    document.getElementById('root'));
