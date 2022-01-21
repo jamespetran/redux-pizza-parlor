11bb4dffc2bd63784d9c436789504d73ea190dc2
James Petran <jm.ptrn@gmail.com>
2022-01-21 11:16:01 -0600

    made the total price work

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,37 +5,55 @@ import { HashRouter as Router, Route, Link } from 'react-router-dom';
 import Checkout from '../Checkout/Checkout'
 import PizzaList from '../PizzaList/PizzaList'
 import CustomerForm from '../CustomerForm/CustomerForm';
-import AdminPage from '../AdminPage/AdminPage'
+import AdminPage from '../AdminPage/AdminPage';
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
 
 function App() {
+  let cart = useSelector(storeInstance => storeInstance.cartReducer)
+
+  let total = () => {
+    if (cart.length === 0) {
+      console.log('cart empty: total=', total)
+      return 0;
+    } else {
+      let newTotal = 0
+      for (let pizza of cart) {
+        newTotal = Number(newTotal) + Number(pizza.price * pizza.qty);
+        console.log('cart NOT empty: total=', total, newTotal)
+      }
+      return newTotal;
+    }
+  }
 
   return (
     <div className='App'>
       <header className='App-header'>
         <h1 className='App-title'>Prime Pizza</h1>
+        <h3>Total: ${total()} </h3>
       </header>
 
-  <Router>
+      <Router>
 
-    <Route path="/" exact>
-      <PizzaList />
-    </Route>
+        <Route path="/" exact>
+          <PizzaList />
+        </Route>
 
-    <Route path="/customerForm" exact>
-      <CustomerForm />
-    </Route>
+        <Route path="/customerForm" exact>
+          <CustomerForm />
+        </Route>
 
-    <Route path="/checkout" exact>
-      <Checkout />
-    </Route>
+        <Route path="/checkout" exact>
+          <Checkout />
+        </Route>
 
-    <Route path="/admin" exact>
-      <AdminPage />
-    </Route>
+        <Route path="/admin" exact>
+          <AdminPage />
+        </Route>
 
-  </Router>
+      </Router>
 
-  
+
     </div >
   );
 }
