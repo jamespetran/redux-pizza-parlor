fe1cd41b5573278c3d2d74b7b8f5adff1b590cf0
James Petran <jm.ptrn@gmail.com>
2022-01-21 12:01:51 -0600

    changed qty to quantity

--- src/index.js
+++ src/index.js
@@ -37,18 +37,18 @@ const cartReducer = (state = [], action) => {
   if (action.type === 'ADD_PIZZA') {
     let exists = checkID(action.payload, state);
     if (state === []) {
-      return [{...pizzaToAdd, qty: 1 }];
+      return [{...pizzaToAdd, quantity: 1 }];
     } else if (exists) {
       return state
-        .map(item => (item.id === action.payload.id ? { ...item, qty: item.qty + 1 } : item))
+        .map(item => (item.id === action.payload.id ? { ...item, quantity: item.quantity + 1 } : item))
     } else if (!exists) {
-      const pizzaToAdd = { ...action.payload, qty: 1 }
+      const pizzaToAdd = { ...action.payload, quantity: 1 }
       return [...state, pizzaToAdd];
     }
   } else if (action.type === 'REMOVE_PIZZA') {
     return state 
-      .map(item => (item.id === action.payload.id ? { ...item, qty: item.qty - 1 } : item))
-      .filter(item => item.qty > 0);
+      .map(item => (item.id === action.payload.id ? { ...item, quantity: item.quantity - 1 } : item))
+      .filter(item => item.quantity > 0);
       //this remove code is from https://stackoverflow.com/a/53892395
   }
   return state;
